0e55bf851f253f8e8c87e5bf0aa2447c90622368
juancedeno96 <cedenojj7@gmail.com>
2021-06-12 01:30:13 -0600

    setup

--- server/index.js
+++ server/index.js
@@ -4,9 +4,27 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const dotenv = require("dotenv");
+const userRoute = require("./routes/users")
+const authRoute = require("./routes/auth")
 
 dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true}, ()=>{
+    console.log('connected to MongoDB')
+});
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users", userRoute)
+app.use("/api/auth", authRoute)
+
+
 
 app.listen(8800, () => {
   console.log('Backend is running');
 });
+
+
