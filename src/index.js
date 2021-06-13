2d5d0ecc54a91614d7d91d3af99a024ba50dc6ae
juancedeno96 <cedenojj7@gmail.com>
2021-06-13 00:15:21 -0600

    added HashRouter and Provider

--- src/index.js
+++ src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter } from "react-router-dom";
+import store from './redux/store'
+import {Provider} from 'react-redux'
+
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
+      <HashRouter>
     <App />
+
+      </HashRouter>
+
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
