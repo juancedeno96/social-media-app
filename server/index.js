a06a441286de3a4096270e57bc797b8a5502da7c
juancedeno96 <cedenojj7@gmail.com>
2021-06-12 22:14:57 -0600

    updated

--- server/index.js
+++ server/index.js
@@ -1,25 +1,16 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
 const morgan = require("morgan");
-const helmet = require("helmet");
 const dotenv = require("dotenv");
-const userRoute = require("./routes/users")
-const authRoute = require("./routes/auth")
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true}, ()=>{
-    console.log('connected to MongoDB')
-});
 
 //middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
-app.use("/api/users", userRoute)
-app.use("/api/auth", authRoute)
 
 
 
