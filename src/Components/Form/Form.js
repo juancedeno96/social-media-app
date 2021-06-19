5a2e0ed0f31f44dfd74616a0705ca13222aa0bd4
juancedeno96 <cedenojj7@gmail.com>
2021-06-19 01:05:24 -0600

    imported scss file

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import noImage from '../../images/no_image.jpg'
+import './Form.scss'
 
 class Form extends Component {
   constructor(props) {
@@ -24,25 +25,19 @@ class Form extends Component {
     let imgSrc = this.state.img ? this.state.img : noImage;
 
     return (
-      <div className='form-content-box'>
+      <div className='Form'>
+        <div className='form-content-box'>
         <h2 className='title'>New Post</h2>
-        <div className='form-main'>
-          <div className='form-input-box'>
             <p>Title:</p>
             <input value={this.state.title} onChange={e => this.setState({ title: e.target.value })} />
-          </div>
           <img className='form-img-prev' src={imgSrc} alt='preview'/>
-          <div className='form-input-box'>
             <p>Image URL:</p>
             <input value={this.state.img} onChange={e => this.setState({ img: e.target.value })} />
-          </div>
-          <div className='form-text-box'>
             <p>Content:</p>
             <textarea value={this.state.content} onChange={e => this.setState({ content: e.target.value })} />
+        <button onClick={this.submit} className='dark-button'>Post</button>
           </div>
         </div>
-        <button onClick={this.submit} className='dark-button'>Post</button>
-      </div>
     );
   }
 }
